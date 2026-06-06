4c0d33e027bf9b4ac80c8bf7541c349268bcfe5a
benjamincarrasco02-lab <benjamin.carrasco02@alumnos.ucn.cl>
2026-06-06 16:02:42 -0400

    mega terminación de Analista suprema

--- src/taller3/Analista.java
+++ src/taller3/Analista.java
@@ -8,16 +8,70 @@ public class Analista implements SistemaAn {
 	Scanner scanner = new Scanner(System.in);
 	ArrayList<Hechizo> listaHechizos = LectorArchivos.lectorHechizos();
 	ArrayList<Mago> listaMagos = LectorArchivos.lectorMagos();
+	
+	
 
+	private double puntajeMago(Mago mago) {
+	    double total = 0;
+
+	    for (String nombreHechizo : mago.getHechizos()) {
+	        for (Hechizo h : listaHechizos) {
+	            if (h.getNombre().equals(nombreHechizo)) {
+	                total += h.calcularPuntaje();
+	            }
+	        }
+	    }
+
+	    return total;
+	}
+	
+	
 	@Override
 	public void mejores10hechizos() {
+		ArrayList<Hechizo> copia = new ArrayList<>();
+
+		for (Hechizo h : listaHechizos) {
+		    copia.add(h);
+		}
 		
+		for (int i = 0; i < copia.size() - 1; i++) {
+		    for (int j = 0; j < copia.size() - 1 - i; j++) {
+		        if (copia.get(j).calcularPuntaje() < copia.get(j + 1).calcularPuntaje()) {
+		            Hechizo aux = copia.get(j);
+		            copia.set(j, copia.get(j + 1));
+		            copia.set(j + 1, aux);
+		        }
+		    }
+		}
+		
+		for (int i = 0; i < 10; i++) {
+		    System.out.println((i + 1) + ") "+ copia.get(i).getNombre()+ " " + copia.get(i).calcularPuntaje());
+		}
 		
 	}
 
 	@Override
 	public void mejores3magos() {
+		ArrayList<Mago> copia = new ArrayList<>();
+
+		for (Mago h : listaMagos) {
+		    copia.add(h);
+		}
 		
+		for (int i = 0; i < copia.size() - 1; i++) {
+		    for (int j = 0; j < copia.size() - 1 - i; j++) {
+		        if (puntajeMago(copia.get(j)) < puntajeMago(copia.get(j +1))) {
+		            Mago aux = copia.get(j);
+		            copia.set(j, copia.get(j + 1));
+		            copia.set(j + 1, aux);
+		        }
+		    }
+		}
+		
+		for (int i = 0; i < 3; i++) {
+		    System.out.println((i + 1) + ") "+ copia.get(i).getNombre()+ " " + puntajeMago(copia.get(i)));
+		}
+		System.out.println("");
 		
 	}
 
@@ -40,19 +94,21 @@ public class Analista implements SistemaAn {
 
 	@Override
 	public void mostrarHechizosYPutuacion() {
-		
+		for(Hechizo h : listaHechizos){
+			System.out.println(h.getNombre() + " -> " + h.calcularPuntaje());
+		}
 		
 	}
 
 	@Override
 	public void mostrarMagosYPuntuacion() {
-		
-		
-	}
-
-	@Override
-	public void calculoPuntaje() {
+		for(Mago m : listaMagos){
+			System.out.println(m.getNombre() + " -> " + puntajeMago(m));
+		}
 		
 	}
 
+
+	
+	
 }
