45c68a86e3d0a44477a0abbc3058a03ba514cb21
benjamincarrasco02-lab <benjamin.carrasco02@alumnos.ucn.cl>
2026-06-07 20:29:16 -0400

    se agregaron comentarios

--- src/taller3/SistemaAn.java
+++ src/taller3/SistemaAn.java
@@ -2,11 +2,11 @@ package taller3;
 
 public interface SistemaAn {
 
-	void mejores10hechizos();
-	void mejores3magos();
-	void mostrarTodosHechizos();
-	void mostrarTodosMagos();
-	void mostrarHechizosYPutuacion();
-	void mostrarMagosYPuntuacion();
+	void mejores10hechizos();//ordena y muestra los 10 mejores hechizos
+	void mejores3magos();//ordena y muestra los 3 mejores magos
+	void mostrarTodosHechizos();//muestra todos los hechizos
+	void mostrarTodosMagos();//muestra todos los magos
+	void mostrarHechizosYPutuacion();//muestra los hechizos y la puntuacion
+	void mostrarMagosYPuntuacion();//muestra los magos y la puntuacion
 	
 }
